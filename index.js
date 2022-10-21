441b4d687a5bed3e37b241e26d445d2b274b1338
jobum97 <brian654321@naver.com>
2022-10-21 12:22:05 +0900

    lambda image resizer

---
+++ index.js
@@ -0,0 +1,40 @@
+const sharp = require("sharp");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+
+const Bucket = "withyou-resource.teamonewayticket";
+const transforms = [
+  { name: "w_480", width: 480 },
+  { name: "w_960", width: 960 }
+];
+
+exports.handler = async (event, context, callback) => {
+
+  const key = event.Records[0].s3.object.key;
+  const sanitizedKey = key.replace(/\+/g, " ");
+  const parts = sanitizedKey.split("/");
+  const resizedDirArr = parts.slice(1,parts.length);
+  const resizedDir = resizedDirArr.join('/');
+  console.log('resizedDir: '+resizedDir);
+  try {
+    const image = await s3.getObject({ Bucket, Key: sanitizedKey }).promise();
+
+    await Promise.all(
+      transforms.map(async item => {
+        const resizedImg = await sharp(image.Body)
+          .resize({ width: item.width })
+          .toBuffer();
+        return await s3
+          .putObject({
+            Bucket,
+            Body: resizedImg,
+            Key: `resized/${item.name}/${resizedDir}`
+          })
+          .promise();
+      })
+    );
+    callback(null, `Success: ${filename}`);
+  } catch (err) {
+    callback(`Error resizing files: ${err}`);
+  }
+};
